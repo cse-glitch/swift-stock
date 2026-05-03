7fc6afb7d99c7ddd80115a61ceb0c93ce75f9326
cse-glitch <en.samiul.cse@gmail.com>
2026-05-03 17:40:51 +0600

    Fix: update application icon to SAMAN logo

--- electron/main.cjs
+++ electron/main.cjs
@@ -6,7 +6,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, '../public/logo.svg'),
+    icon: path.join(__dirname, '../public/icon.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
