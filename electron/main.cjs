fcdf4373e112516ff525c79eb8d1494be69f6835
cse-glitch <en.samiul.cse@gmail.com>
2026-05-03 17:49:21 +0600

    Revert: use logo.svg for application icon

--- electron/main.cjs
+++ electron/main.cjs
@@ -6,7 +6,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, '../public/icon.png'),
+    icon: path.join(__dirname, '../public/logo.svg'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
